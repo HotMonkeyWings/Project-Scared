f10ed6ff38c432b4bef4c1904c65dfde5e05ba91
EpicBruh <devsony52@gmail.com>
2020-07-01 17:20:33 +0530

    Added dialogue system

---
+++ I'm Scared/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public Dialogue success;
+    public int NoOfPills = 0;
+    public bool pillInside = false;
+    
+
+    public void TriggerDialogue() => FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    public void TriggerDialogueSuccess() => FindObjectOfType<DialogueManager>().StartDialogue(success);
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && collision.CompareTag("Player") && pillInside)
+        {
+
+            collision.GetComponent<PillSystem>().addPill(NoOfPills);
+            TriggerDialogueSuccess();
+            pillInside = false;
+            NoOfPills = 0;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && collision.CompareTag("Player") && !pillInside)
+            TriggerDialogue();
+    }
+
+}
